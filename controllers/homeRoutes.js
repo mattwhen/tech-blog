82df82d07ee03ea231f508d0b43780386bd6ea58
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-04-19 07:00:30 -0500

    this commit adds missing associations in the models/index.js file, comments out the withAuth middleware functionality, and adding content to the homepage.hbs file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -39,7 +39,7 @@ router.get('/post/:id', async (req, res) => {
     });
     const post = postData.get({ plain: true });
 
-    res.render('post', {
+    res.render('singlepost', {
       ...post,
       logged_in: req.session.logged_in
     });
@@ -48,34 +48,34 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
+// // Use withAuth middleware to prevent access to route
+// router.get('/profile', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: Project }],
+//     });
 
-    const user = userData.get({ plain: true });
+//     const user = userData.get({ plain: true });
 
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render('profile', {
+//       ...user,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/profile'); // Redirects user to a different URL
     return;
   }
 
-  res.render('login');
+  res.render('login'); // Renders a view template and send the HTML result to the client browser.
 });
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -24,4 +24,12 @@ Comment.belongsTo(Post, {
     foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Comment, Post };
